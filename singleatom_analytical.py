1529b4b1cd690eb3aeb94c9f86705484631081f7
Oliver Dudgeon <oliverdudgeon@gmail.com>
2020-03-04 11:47:11 +0000

    Fix slicing of prob amplitudes

--- singleatom_analytical.py
+++ singleatom_analytical.py
@@ -23,8 +23,8 @@ t = np.array([np.linspace(0, 10, 1_000)]).T
 cs = k1 * np.exp( -1j*eigenvalues[0] * t) * np.array([eigenvectors[0]]) + \
     k2 * np.exp( -1j*eigenvalues[1] * t) * np.array([eigenvectors[1]])
 
-plt.plot(t, np.abs(cs[0, :])**2, label='$|c_1|^2$')
-plt.plot(t, np.abs(cs[1, :])**2, label='$|c_r|^2$')
+plt.plot(t, np.abs(cs[:, 0])**2, label='$|c_1|^2$')
+plt.plot(t, np.abs(cs[:, 1])**2, label='$|c_r|^2$')
 plt.plot(t, np.sum(np.abs(cs)**2, axis=1), label='normalisation')
 
 plt.xlabel('Time, $t$')
