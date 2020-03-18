0046bf64be5492a51e7551127ebec0744925c305
tizayi <57833318+tizayi@users.noreply.github.com>
2020-03-18 12:29:56 +0000

    Update singleatom_twoatom_compare.py

--- singleatom_twoatom_compare.py
+++ singleatom_twoatom_compare.py
@@ -6,21 +6,23 @@ from twoatom_numerical import solve_with as solve_twoatom
 
 sol1 = solve_singleatom(laser_freq=1, detuning=0.5, tf=10, init=[1+0j, 0+0j])
 sol2 = solve_twoatom(laser=1, detuning_1=0.5,
-                     detuning_2=0.5, tf=10, init=[0+0j, 0+0j, 0+0j, 1+0j], V=0)
+                     detuning_2=0.5, tf=10, init=[1+0j, 0+0j, 0+0j, 0+0j], V=0)
 
-plt.plot(sol1.t, np.abs(sol1.y[0])**2, label='$|c_1|^2$')
-plt.plot(sol1.t, np.abs(sol1.y[1])**2, label='$|c_r|^2$')
+fig, axs = plt.subplots(2, sharex=True, sharey=True) #Creates a figure window 
 
+axs[0].plot(sol1.t, np.abs(sol1.y[0])**2, label='$|c_1|^2$')
+axs[0].plot(sol1.t, np.abs(sol1.y[1])**2, label='$|c_r|^2$')
 
 # Combined Prob. Amps
-plt.plot(sol2.t, np.abs(sol2.y[0])**2 + np.abs(sol2.y[2]) ** 2,
+axs[1].plot(sol2.t, np.abs(sol2.y[0])**2 + np.abs(sol2.y[2]) ** 2,
          label='$|c_{11}|^2 + |c_{r1}|^2$', ls='--')
-plt.plot(sol2.t, np.abs(sol2.y[1])**2 + np.abs(sol2.y[3]) ** 2,
+axs[1].plot(sol2.t, np.abs(sol2.y[1])**2 + np.abs(sol2.y[3]) ** 2,
          label='$|c_{1r}|^2 + |c_{rr}|^2$', ls='--')
 
-plt.xlabel('Time, $t$')
-plt.ylabel('Probability Amplitude')
-plt.legend(frameon=False, ncol=5, loc='upper center',
-           bbox_to_anchor=(0.5, 1.1))
-plt.savefig('single_twoatom_compare.png', dpi=100)
+axs[0].set(xlabel='Time, $t$',ylabel='Probability Amplitude')
+axs[1].set(xlabel='Time, $t$',ylabel='Probability Amplitude')
+
+axs[0].legend(frameon='false',ncol=5, loc='upper right')
+axs[1].legend(frameon='false',ncol=5, loc='upper right')
+#plt.savefig('single_twoatom_compare.png', dpi=100)
 plt.show()
